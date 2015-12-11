251cfa3150cce13f87d72bd514228a452f6aaa9a
Jacob Walker <jacobwalker0814@gmail.com>
2015-12-11 16:11:02 -0600

    Clean up main

--- day10/main.go
+++ day10/main.go
@@ -6,9 +6,23 @@ import (
 )
 
 func main() {
-	t0 := time.Now()
-	fmt.Println("AOC Day 10 Part 2")
-	fmt.Println("50 iterations: ")
-	fmt.Println(len(lookAndSay("1113122113", 50)))
-	fmt.Println("That took", time.Since(t0))
+	fmt.Println("AOC Day 10")
+
+	run("Part 1", 40)
+	run("Part 2", 50)
+}
+
+func run(label string, iterations int) {
+	var (
+		start    time.Time
+		duration time.Duration
+		val      int
+		input    = "1113122113"
+	)
+
+	fmt.Printf("%s: %d iterations: ", label, iterations)
+	start = time.Now()
+	val = len(lookAndSay(input, iterations))
+	duration = time.Since(start)
+	fmt.Println(val, duration)
 }
