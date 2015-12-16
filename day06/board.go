82f53d206a0062c323dec84394cf1648e905d476
Jacob Walker <jacobwalker0814@gmail.com>
2015-12-16 14:36:12 -0600

    Give lights some padding

--- day06/board.go
+++ day06/board.go
@@ -7,7 +7,20 @@ import (
 )
 
 // r is used by Board for creating images
-var r = image.Rect(0, 0, 1000, 1000)
+// Lights have some padding and take 4 pixels each plus some extra padding on
+// the right and bottom. So 2 rows of 2 lights would look like:
+//
+// yx01234
+// 0 .....
+// 1 .*.*.
+// 2 .....
+// 3 .*.*.
+// 4 .....
+//
+// Therefore 1000 lights across will be 2000 pixels wide plus 1 pixel for the
+// last column's right padding. Similarly 1000 lights tall will be 2000 pixels
+// high plus 1 pixel for the last row's bottom padding.
+var r = image.Rect(0, 0, 2001, 2001)
 
 // Board is a collection of lights
 type Board map[Point]*Light
@@ -29,7 +42,7 @@ func (b Board) ImageBW() *image.Paletted {
 	img := image.NewPaletted(r, palette.Plan9)
 	for p, l := range b {
 		if l.On {
-			img.Set(p.X, p.Y, color.White)
+			img.Set(p.X*2+1, p.Y*2+1, color.White)
 		}
 	}
 
@@ -40,7 +53,7 @@ func (b Board) ImageBW() *image.Paletted {
 func (b Board) ImageColor() *image.Paletted {
 	img := image.NewPaletted(r, palette.Plan9)
 	for p, l := range b {
-		img.Set(p.X, p.Y, l.Color())
+		img.Set(p.X*2+1, p.Y*2+1, l.Color())
 	}
 
 	return img
--- day06/main.go
+++ day06/main.go
@@ -8,14 +8,12 @@ import (
 
 func main() {
 	var (
-		b         = NewBoard()
-		commands  = ParseCommands("input.txt")
-		doMakeGif = true
-		g1        = NewGIF("lights_1.gif")
-		g2        = NewGIF("lights_2.gif")
+		b        = NewBoard()
+		commands = ParseCommands("input.txt")
+		g1       = NewGIF("lights_1.gif")
+		g2       = NewGIF("lights_2.gif")
 	)
 
-	commands = commands[0:30]
 	fmt.Println("Processing commands")
 	bar := pb.StartNew(len(commands))
 	for _, cmd := range commands {
@@ -23,19 +21,15 @@ func main() {
 			b[p].TakeAction(cmd.Action)
 		}
 
-		if doMakeGif {
-			g1.AddImage(b.ImageBW())
-			g2.AddImage(b.ImageColor())
-		}
+		g1.AddImage(b.ImageBW())
+		g2.AddImage(b.ImageColor())
 		bar.Increment()
 	}
 
 	bar.FinishPrint("Wrapping up")
 
-	if doMakeGif {
-		g1.Encode()
-		g2.Encode()
-	}
+	g1.Encode()
+	g2.Encode()
 
 	fmt.Println("Lights on at the end", b.LitCount()) // 569999 for me
 	fmt.Println("Total Brightness", b.Brightness())   // 17836115 for me
