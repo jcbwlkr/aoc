156182e5069883d5b778de76e70631effefd4673
Jacob Walker <jacobwalker0814@gmail.com>
2015-12-22 15:58:02 -0600

    Day 12 part 2 and better part 1

--- day12/main.go
+++ day12/main.go
@@ -1,33 +1,51 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"regexp"
-	"strconv"
+	"os"
 )
 
 func main() {
-	nums := regexp.MustCompile("([-0-9]+)")
-
-	input, err := ioutil.ReadFile("input.json")
+	file, err := os.Open("input.json")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var sum int
-	matches := nums.FindAllStringSubmatch(string(input), -1)
-	fmt.Println(matches)
-	for _, m := range matches {
-		for i := 1; i < len(m); i++ {
-			n, err := strconv.Atoi(m[i])
-			if err != nil {
-				log.Fatal(err)
-			}
-			sum += n
-		}
+	var data interface{}
+	if err := json.NewDecoder(file).Decode(&data); err != nil {
+		log.Fatal(err)
 	}
 
-	fmt.Println(sum)
+	fmt.Println("AoC Day 12")
+	fmt.Print("Part 1: ")
+	fmt.Println(parse(data, true))
+	fmt.Print("Part 2: ")
+	fmt.Println(parse(data, false))
+}
+
+func parse(data interface{}, redOK bool) float64 {
+	switch t := data.(type) {
+	case float64:
+		return t
+	case string:
+		return 0
+	case []interface{}:
+		var sum float64
+		for _, d := range t {
+			sum += parse(d, redOK)
+		}
+		return sum
+	case map[string]interface{}:
+		var sum float64
+		for _, d := range t {
+			if d == "red" && !redOK {
+				return 0
+			}
+			sum += parse(d, redOK)
+		}
+		return sum
+	}
+	return 0
 }
