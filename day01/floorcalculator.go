8f6113aafa61410a54d47e4a64155e074ac6bc30
Jacob Walker <jacobwalker0814@gmail.com>
2015-12-23 12:22:42 -0600

    AoC Day 1: Use bufio.Scanner instead of io.Reader directly

--- day01/floorcalculator.go
+++ day01/floorcalculator.go
@@ -1,24 +1,21 @@
 package main
 
-import "io"
+import (
+	"bufio"
+	"io"
+)
 
 func floorCalculator(r io.Reader) int {
-	var (
-		scratch = make([]byte, 32)
-		floor   int
-		read    int
-	)
-	for {
-		if read, _ = r.Read(scratch); read == 0 {
-			break
-		}
-		for i := 0; i < read; i++ {
-			switch scratch[i] {
-			case '(':
-				floor++
-			case ')':
-				floor--
-			}
+	var floor int
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanBytes)
+
+	for s.Scan() {
+		switch s.Text() {
+		case "(":
+			floor++
+		case ")":
+			floor--
 		}
 	}
 
@@ -27,26 +24,22 @@ func floorCalculator(r io.Reader) int {
 
 func basementFinder(r io.Reader) int {
 	var (
-		scratch = make([]byte, 32)
-		floor   int
-		read    int
-		index   int
+		floor int
+		index int
 	)
-	for {
-		if read, _ = r.Read(scratch); read == 0 {
-			break
+	s := bufio.NewScanner(r)
+	s.Split(bufio.ScanBytes)
+
+	for s.Scan() {
+		index++
+		switch s.Text() {
+		case "(":
+			floor++
+		case ")":
+			floor--
 		}
-		for i := 0; i < read; i++ {
-			index++
-			switch scratch[i] {
-			case '(':
-				floor++
-			case ')':
-				floor--
-			}
-			if floor == -1 {
-				return index
-			}
+		if floor == -1 {
+			return index
 		}
 	}
 
