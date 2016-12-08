428ae0867d1d88cf563b25617866b0223406f132
Jacob Walker <jacobwalker0814@gmail.com>
2016-12-08 11:02:39 -0500

    AoC 2016 Day 5: D Cleanup

--- 2016/day05/source/app.d
+++ 2016/day05/source/app.d
@@ -16,28 +16,33 @@ void main()
         ubyte[] hash = md5.digest(id ~ to!string(index));
         string hashS = toHexString(hash[0..4]);
 
-        if(hashS[0..5] == "00000") {
-            if (code1.length < 8) {
-                code1 ~= hashS[5..6];
-                writeln("Part 1: ", index, " -- ", code1);
-            }
-
-            if (found2 >= 8) {
-                continue;
-            }
-            try {
-                int pos = to!int(hashS[5..6]);
-                if (pos < 8 && code2[pos] == 0xFF) {
-                    found2++;
-                    code2[pos] = to!char(hashS[6..7]);
-                    writeln("Part 2: ", index, " -- ", code2, " found: ", found2);
-                }
-            } catch (Exception e) {
-                // Converting a character like "E" to int failed
-            }
+        index++;
+        if(hashS[0..5] != "00000") {
+            continue;
         }
 
-        index++;
+        if (code1.length < 8) {
+            code1 ~= hashS[5..6];
+            writeln("Part 1: ", index, " -- ", code1);
+        }
+
+        if (found2 >= 8) {
+            continue;
+        }
+
+        int pos;
+        try {
+            pos = to!int(hashS[5..6]);
+        } catch (Exception e) {
+            // Converting a character like "E" to int failed
+            continue;
+        }
+
+        if (pos < 8 && code2[pos] == 0xFF) {
+            found2++;
+            code2[pos] = to!char(hashS[6..7]);
+            writeln("Part 2: ", index, " -- ", code2, " found: ", found2);
+        }
     }
 
     writeln("The first code is  ", toLower(code1));
